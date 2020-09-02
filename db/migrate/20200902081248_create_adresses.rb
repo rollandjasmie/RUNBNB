b5ebcda2e8e5bcdd377a79d9c0962fa12fd601a1
safidypatrick <safidypatrick8gmail.com>
2020-09-02 13:04:05 +0300

    info

--- db/migrate/20200902081248_create_adresses.rb
+++ db/migrate/20200902081248_create_adresses.rb
@@ -1,12 +1,12 @@
 class CreateAdresses < ActiveRecord::Migration[6.0]
   def change
     create_table :adresses do |t|
-      t.string :pays
-      t.string :adresse
-      t.integer :code
-      t.string :ville
+    	t.string :pays
+    	t.string :adresse
+    	t.integer :code
+    	t.string :ville
 
-      t.belongs_to :logement, index: true
+    	t.belongs_to :logement, index: true
       t.timestamps
     end
   end
