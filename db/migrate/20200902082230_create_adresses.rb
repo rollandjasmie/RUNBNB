b2ec16e517efc9f17cbf066873a88a05c14d71e2
safidypatrick <safidypatrick8gmail.com>
2020-09-02 11:28:10 +0300

    adresse

---
+++ db/migrate/20200902082230_create_adresses.rb
@@ -0,0 +1,13 @@
+class CreateAdresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :adresses do |t|
+      t.string :pays
+      t.string :adresse
+      t.integer :code
+      t.string :ville
+
+      t.belongs_to :logement, index: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_135903) do
+ActiveRecord::Schema.define(version: 2020_09_02_082230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,9 +41,9 @@ ActiveRecord::Schema.define(version: 2020_09_01_135903) do
     t.string "adresse"
     t.integer "code"
     t.string "ville"
+    t.bigint "logement_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "logement_id"
     t.index ["logement_id"], name: "index_adresses_on_logement_id"
   end
 
@@ -110,5 +110,4 @@ ActiveRecord::Schema.define(version: 2020_09_01_135903) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "adresses", "logements"
 end
