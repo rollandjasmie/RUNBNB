40d401df8ed774d6e0bedb1fcf4f097ab5d3d5c5
safidypatrick <safidypatrick8gmail.com>
2020-09-21 11:04:16 +0300

    cal

--- db/migrate/20200908064323_create_reservations.rb
+++ db/migrate/20200908064323_create_reservations.rb
@@ -1,8 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[6.0]
   def change
     create_table :reservations do |t|
-      t.datetime :start_date
-      t.datetime :end_date
+      t.string :start_date
+      t.string :end_date
       t.belongs_to :logement
       t.timestamps
     end
