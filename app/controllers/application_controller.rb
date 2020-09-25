3fe013288e03085ae765210ff5ef6fd36adf244e
iantsaraz <iantsaraz02@gmail.com>
2020-09-25 23:17:07 +0300

    fafana

--- app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  before_action :configure_devise_parameters, if: :devise_controller?
-
-  def configure_devise_parameters
-    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name,:sexe, :name,:mobile,:date_of_birth,:adresse, :email,:urgence, :password, :password_confirmation)}
-    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name,:sexe, :name,:mobile,:date_of_birth,:adresse, :email,:urgence, :password, :current_password)}
-  end
-end
